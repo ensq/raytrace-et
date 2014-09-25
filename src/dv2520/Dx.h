d784802ec8ad1f2558c64012afb3ca68e12e703e
CaterHatterPillar <CaterHatterPillar@gmail.com>
2014-09-25 08:37:38 +0200

    User-defined white color is now actually white.

--- src/dv2520/Dx.h
+++ src/dv2520/Dx.h
@@ -23,13 +23,12 @@ class TimerD3d;
 #include <geometry.h>
 #include <BufStreamSrv.h>
 
-namespace DxClearColor {
-const FLOAT Black[ 4 ] = { 0.0f, 0.0f, 0.0f, 1.0f };
-const FLOAT White[ 4 ] = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-const FLOAT Red[ 4 ] = { 1.0f, 0.0f, 0.0f, 1.0f };
-const FLOAT Green[ 4 ] = { 0.0f, 1.0f, 0.0f, 1.0f };
-const FLOAT Blue[ 4 ] = { 0.0f, 0.0f, 1.0f, 1.0f };
+namespace DxClearColor {    
+    const FLOAT Red  [4] = {1.0f, 0.0f, 0.0f, 1.0f};
+    const FLOAT Green[4] = {0.0f, 1.0f, 0.0f, 1.0f};
+    const FLOAT Blue [4] = {0.0f, 0.0f, 1.0f, 1.0f};
+    const FLOAT Black[4] = {0.0f, 0.0f, 0.0f, 1.0f};
+    const FLOAT White[4] = {1.0f, 1.0f, 1.0f, 1.0f};
 }
 
 class Dx {
